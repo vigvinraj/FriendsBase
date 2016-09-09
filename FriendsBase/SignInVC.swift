0564e417b5c5f24b09b2577e58795e450debcfae
vigvinraj <user@users-MacBook-Pro.local>
2016-09-09 15:01:59 -0700

    Sucessfully Completed Facebook and Firebase Authentication

--- FriendsBase/SignInVC.swift
+++ FriendsBase/SignInVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+
 
 class SignInVC: UIViewController {
 
@@ -15,11 +19,33 @@ class SignInVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with Facebook - \(error)")
+            }else if result?.isCancelled == true {
+                print("User cancelled Facebook Authentication")
+            }else {
+                print("Sucessfully Authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
     }
-
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("Sucessfully Authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
--- Podfile
+++ Podfile
@@ -10,6 +10,7 @@ target 'FriendsBase' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'FBSDKLoginKit'
   
 
 end
