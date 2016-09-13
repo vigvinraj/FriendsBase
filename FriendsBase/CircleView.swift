59560a02fb0d90c161730d89dcd20b116d115d0d
vigvinraj <user@users-MacBook-Pro.local>
2016-09-13 12:31:04 -0700

    Completed the Feed UI

--- FriendsBase/CircleView.swift
+++ FriendsBase/CircleView.swift
@@ -1,22 +1,27 @@
 //
-//  DesignView.swift
+//  CircleView.swift
 //  FriendsBase
 //
-//  Created by Vignesh Kumar on 9/9/16.
+//  Created by Vignesh Kumar on 9/13/16.
 //  Copyright © 2016 Vignesh Kumar. All rights reserved.
 //
 
 import UIKit
 
-class DesignView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 5.0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+        
     }
 }
