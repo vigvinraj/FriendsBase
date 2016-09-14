d6a0be39f83cff7c4fbfd9a4eba653688461f321
vigvinraj <user@users-MacBook-Pro.local>
2016-09-14 09:32:21 -0700

    Completed creating Firebase DB Users

--- FriendsBase/SignInVC.swift
+++ FriendsBase/SignInVC.swift
@@ -17,7 +17,7 @@ class SignInVC: UIViewController {
 
     @IBOutlet weak var emailField: DesignTxtField!
     @IBOutlet weak var passwordField: DesignTxtField!
-    
+    @IBOutlet weak var emailWarningLbl: UILabel!
     @IBOutlet weak var pwdWarningLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,7 +56,8 @@ class SignInVC: UIViewController {
             } else {
                 print("Sucessfully Authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -69,8 +70,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("Email User Authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
-                    }
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)                    }
 
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -78,12 +79,20 @@ class SignInVC: UIViewController {
                             if pwd.characters.count < 6 {
                                 self.pwdWarningLbl.isHidden = false
                             }
+                            if error.debugDescription.contains("email") == true || email.characters.count < 1 {
+                                self.emailWarningLbl.isHidden = false
+                                if error.debugDescription.contains("already in use") == true {
+                                    self.emailWarningLbl.text = "* Email Already in Use"
+                                    self.emailWarningLbl.isHidden = false
+                                }
+                            }
 
                             print("Unable to Authenticate with Firebase using email - \(error)")
                         } else {
                             print("Sucessfully authenticated with Firbase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
 
                         }
@@ -95,7 +104,9 @@ class SignInVC: UIViewController {
         
     }
     
-    func completeSignIn(id: String) {
+    
+    func completeSignIn(id: String, userData: Dictionary<String,String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keyChainResult = KeychainWrapper.defaultKeychainWrapper().setString(id, forKey: KEY_UID)
         print("Data saved to keychain \(keyChainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
